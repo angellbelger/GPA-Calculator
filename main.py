3e1ad88fc993e13690b726ad173ec5f023e5ee7e
Angell <angellbelger@hotmail.com>
2021-05-03 13:57:32 -0300

    hello

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from utl.lay import title, colour, line, options
 from utl.arch import readfloat, readint, grade, select, points, credits, all, calgpa, txt, gradeusa, allusa
 
+print('{}Hello, world{}'.format('\033[36m', '\033[m'))
